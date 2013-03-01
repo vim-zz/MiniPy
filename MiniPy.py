a20b62652c026c26bfa22106b94c503f4ddecc3d
ofer affias <ofer.affias@gmail.com>
2013-03-01 22:02:29 +0200

    initial release - ported to python3/st3

---
+++ MiniPy.py
@@ -0,0 +1,34 @@
+from math import *
+from random import *
+import re
+import sublime, sublime_plugin
+
+# reverse() in python3 
+def rev(s): return s[::-1]
+
+class Minipy_evalCommand(sublime_plugin.TextCommand):
+	def run(self, edit, user_input=None):
+		self.edit = edit
+		view = self.view
+		script = ""
+
+		# sum the total number of special char $
+		total = 0
+		for region in view.sel():
+			total += view.substr(region).count("$")
+
+		# build a list from 1 to the number of special chars
+		serial_number = list(range(total+1,1))
+		# serial_number.reverse()
+		# serial_number = rev(serial_number)
+		# print(repr(serial_number))
+
+		# replace each special char $ with the next index from serial_number list
+		# and eval the expression
+		for region in view.sel():
+			if region.begin() != region.end():
+				script = view.substr(region)
+				for n in range(script.count("$")):
+					script = re.sub(r"\$", str(serial_number.pop()), script, 1)
+				# print(eval(script))
+				view.replace(edit, region, str(eval(script)))
