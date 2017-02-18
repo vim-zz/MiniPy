6f257955af221e4db5abacfa800d777c55b08935
ofer affias <ofer@3dsig.com>
2017-02-18 23:09:43 +0200

    add support for time conversions

--- MiniPy.py
+++ MiniPy.py
@@ -3,8 +3,40 @@ from random import *
 import re
 import sublime, sublime_plugin
 
+import sys 
+import os
+ # arrow-dists is the folder in our plugin
+sys.path.append(os.path.join(os.path.dirname(__file__), "arrow"))
+import arrow
+
+
+def ts_precision():
+    return 1000.0
+
+
+def t(phrase, tz='-0000'):
+    try:
+        timestamp = int(phrase)
+        return timestamp_to_string(timestamp, tz)
+    except ValueError:
+        return string_to_timestamp(phrase)
+
+
+def timestamp_to_string(timestamp, tz_offset):
+    fmt = 'YYYY-MM-DDTHH:mm:ss.SSSZ'
+    # convert timezone offset to ISO with ':' (we dont use it)
+    if ':' not in tz_offset:
+        tz_offset = ':'.join([tz_offset[0:3], tz_offset[3:5]])
+    return arrow.get(timestamp/ts_precision()).to(tz_offset).format(fmt)
+        
+
+def string_to_timestamp(string):
+    return int(arrow.get(string).float_timestamp * ts_precision())
+
+
 # reverse() in python3 
 def rev(s): return s[::-1]
+
 
 class Minipy_evalCommand(sublime_plugin.TextCommand):
 	def run(self, edit, user_input=None):
@@ -17,12 +49,12 @@ class Minipy_evalCommand(sublime_plugin.TextCommand):
 		for region in view.sel():
 			total += view.substr(region).count("$")
 
-		print("total = ", total)
+		### print("total = ", total)
 		# build a list from 1 to the number of special chars
 		serial_number = list(range(1, total+1))
 		# serial_number.reverse()
 		serial_number = rev(serial_number)
-		print(repr(serial_number))
+		### print(repr(serial_number))
 
 		# replace each special char $ with the next index from serial_number list
 		# and eval the expression
--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 Sublime Text 3 plugin - inline python evaluation.
 
-For example you can write 3.14*0.6 and get the result (1.884) in your text.
-It also supports multiple selection.
+For example you can write `3.14*0.6` and get the result `1.884` right in your open view.
+
+Multiple selection is supported as well.
 
 Another feature is the use of $ as accumolation variable, e.g. having the following multipe selection:
 
@@ -22,6 +23,17 @@ similarly:
 	arr[0 ]      arr[0]
 	arr[$+2]  -> arr[3]
 	arr[$*3]     arr[6]
+
+## Time
+
+The Arrow library is used to evaluate epoch timestamps to strings and vice versa.
+
+To use just wrap your text with `t()`, for example:
+
+	t(1444321015000) -> 2015-10-08T16:16:55.000-0000
+	t('2015-10-08T16:16:55.000-0000') -> 1444321015000
+	# with timezone UTC+6
+	t('2016-02-16T20:14:03.834+0600') -> 1444321015000
 
 ## Usage
 
