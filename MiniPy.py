01eb6fbc4fcc75e7e424cdea2278870717e2d8a1
Allan L. R. Hansen <allan.lrh@gmail.com>
2017-05-02 21:58:51 +0200

    Applied PEP8 formattings

--- MiniPy.py
+++ MiniPy.py
@@ -1,35 +1,40 @@
 from math import *
 from random import *
 import re
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
 
-# reverse() in python3 
-def rev(s): return s[::-1]
+# reverse() in python3
+
+
+def rev(s):
+    return s[::-1]
+
 
 class Minipy_evalCommand(sublime_plugin.TextCommand):
-	def run(self, edit, user_input=None):
-		self.edit = edit
-		view = self.view
-		script = ""
+    def run(self, edit, user_input=None):
+        self.edit = edit
+        view = self.view
+        script = ""
 
-		# sum the total number of special char $
-		total = 0
-		for region in view.sel():
-			total += view.substr(region).count("$")
+        # sum the total number of special char $
+        total = 0
+        for region in view.sel():
+            total += view.substr(region).count("$")
 
-		print("total = ", total)
-		# build a list from 1 to the number of special chars
-		serial_number = list(range(1, total+1))
-		# serial_number.reverse()
-		serial_number = rev(serial_number)
-		print(repr(serial_number))
+        print("total = ", total)
+        # build a list from 1 to the number of special chars
+        serial_number = list(range(1, total + 1))
+        # serial_number.reverse()
+        serial_number = rev(serial_number)
+        print(repr(serial_number))
 
-		# replace each special char $ with the next index from serial_number list
-		# and eval the expression
-		for region in view.sel():
-			if region.begin() != region.end():
-				script = view.substr(region)
-				for n in range(script.count("$")):
-					script = re.sub(r"\$", str(serial_number.pop()), script, 1)
-				# print(eval(script))
-				view.replace(edit, region, str(eval(script)))
+        # replace each special char $ with the next index from serial_number list
+        # and eval the expression
+        for region in view.sel():
+            if region.begin() != region.end():
+                script = view.substr(region)
+                for n in range(script.count("$")):
+                    script = re.sub(r"\$", str(serial_number.pop()), script, 1)
+                # print(eval(script))
+                view.replace(edit, region, str(eval(script)))
